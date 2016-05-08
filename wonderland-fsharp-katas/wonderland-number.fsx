476a5f369b577adff62c89d7db778f204380b45f
Horia Toma <toma.horia@gmail.com>
2016-05-08 21:35:33 +0200

    Solution to wonderland number

--- wonderland-fsharp-katas/wonderland-number.fsx
+++ wonderland-fsharp-katas/wonderland-number.fsx
@@ -1,9 +1,23 @@
 // See the file wonderland-number.md for detailed information.
 
-let wonderlandNumber () = 42
-
 let haveSameDigits (n1:int,n2:int) =
     (string n1 |> Set.ofSeq) = (string n2 |> Set.ofSeq)
+
+let hasUniqueDigits (v: int) =
+    string v
+    |> Seq.groupBy (fun v -> v)
+    |> Seq.exists (fun (k,l) -> l |> Seq.length >1) |> not
+
+let wonderlandNumber () = 
+    [123456..(int (1000000/6))] // first digit has to be 1, otherwise it overflows the six digits
+    |> List.filter hasUniqueDigits
+    |> List.filter (fun v ->
+                            haveSameDigits(v, v*2) &&
+                            haveSameDigits(v, v*3) &&
+                            haveSameDigits(v, v*4) &&
+                            haveSameDigits(v, v*5) &&
+                            haveSameDigits(v, v*6))
+    |> List.head
 
 #r @"../packages/Unquote/lib/net45/Unquote.dll"
 open Swensen.Unquote
